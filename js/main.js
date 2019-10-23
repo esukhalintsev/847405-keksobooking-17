b55f69aa401a4a5f01526811ba13ad4c5c11ed04
Evgeniy <e.sukhalintsev@yandex.ru>
2019-10-23 13:14:45 +0300

    add pins to map

--- js/main.js
+++ js/main.js
@@ -1,64 +1,68 @@
 'use strict';
-
-var APPARTMENT_TYPES = ['palace', 'flat', 'house', 'bungalo'];
-var Mock = {
-  countElements: 8,
-  minY: 130,
-  maxY: 630
-};
-var appartments = [];
 var map = document.querySelector('.map');
-var pinList = map.querySelector('.map__pins');
+map.classList.remove('map--faded');
 
-var pinTemplate = document.querySelector('#pin')
+var map_pins = document.querySelector('.map__pins');
+
+var PIN_WIDTH = 40;
+var PIN_HEIGHT = 44;
+var MIN_X = 320;
+var MAX_X = 1200;
+var MIN_Y = 130;
+var MAX_Y = 630;
+var PINS_COUNT = 8;
+
+var pins = [];
+var TYPES = ['palace', 'flat', 'house', 'bungalo'];
+
+//возвращает случайный элемент из массива
+var getRandomElement = function (arr) {
+  var rand = Math.floor(Math.random() * arr.length);
+  return arr[rand];
+};
+
+//возвращает случайное число из интервала
+var getRandomIntFromInterval = function (min, max) {
+  var rand = Math.floor(Math.random() * (max - min + 1) + min);
+  return rand;
+};
+
+//наполнение массива объектами пинами
+for (var i = 1; i <= PINS_COUNT; i++) {
+  var pin = {
+    author: {
+      avatar: 'img/avatars/user0' + i + '.png',
+      alt: 'Метка объявления №' + i
+    },
+    offer: {
+      type: getRandomElement(TYPES)
+    },
+    location: {
+      x: getRandomIntFromInterval(MIN_X, MAX_X),
+      y: getRandomIntFromInterval(MIN_Y, MAX_Y)
+    },
+  };
+  pins.push(pin);
+}
+
+//поиск темплейта
+var similarPin = document.querySelector('#pin')
   .content
   .querySelector('.map__pin');
 
-var getRandomInt = function (min, max) {
-  return Math.floor(Math.random() * (max - min + 1)) + min;
-};
-
-var addMockElement = function (arr, count, type, x, y) {
-  arr.push({
-    'author': {
-      'avatar': 'img/avatars/user0' + count + '.png'
-    },
-    'offer': {
-      'type': type
-    },
-    'location': {
-      'x': x,
-      'y': y
-    }
-  });
-};
-
-var createMock = function () {
-  for (var i = 1; i <= Mock.countElements; i++) {
-    addMockElement(appartments, i, APPARTMENT_TYPES[getRandomInt(0, APPARTMENT_TYPES.length - 1)], getRandomInt(0, map.clientWidth), getRandomInt(Mock.minY, Mock.maxY));
-  }
-};
-
-var renderPin = function (pin) {
-  var pinElement = pinTemplate.cloneNode(true);
-  var pinImg = pinElement.querySelector('img');
-
-  pinImg.src = pin.author.avatar;
-  pinImg.alt = pin.offer.type;
-  pinElement.style.left = pin.location.x - pinImg.width / 2 + 'px';
-  pinElement.style.top = pin.location.y - pinImg.height + 'px';
-
+//создание пина
+var renderPins = function (pin) {
+  var pinElement = similarPin.cloneNode(true);
+  pinElement.style.left = pin.location.x - PIN_WIDTH / 2 + 'px';
+  pinElement.style.top = pin.location.y - PIN_HEIGHT + 'px';
+  pinElement.querySelector('img').src = pin.author.avatar;
+  pinElement.querySelector('img').alt = pin.author.alt;
   return pinElement;
 };
 
-var createFragment = function () {
-  var fragment = document.createDocumentFragment();
-  for (var j = 0; j < appartments.length; j++) {
-    fragment.appendChild(renderPin(appartments[j]));
-  }
-  return fragment;
-};
-
-createMock();
-map.classList.remove('map--faded');
-pinList.appendChild(createFragment());
+//добавление пинов на карту
+var fragment = document.createDocumentFragment();
+for (var i = 0; i < pins.length; i++) {
+  fragment.appendChild(renderPins(pins[i]));
+}
+map_pins.appendChild(fragment);
